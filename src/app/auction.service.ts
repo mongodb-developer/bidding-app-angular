749b2f2f1aef6b30c849c5bcba2476a717a1affc
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2023-02-24 18:23:08 +0530

    Empty auction service

--- src/app/auction.service.ts
+++ src/app/auction.service.ts
@@ -4,73 +4,125 @@ import { extractUpdate, fromChangeEvent } from './custom-operators';
 import { RealmAppService } from './realm-app.service';
 import { Auction } from './auction';
 import { isInUsernamesList } from './usernames';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
-  constructor(private realmAppService: RealmAppService) {
-  }
+  constructor(private realmAppService: RealmAppService) { }
 
-  async findOne(id: any) {
-    const collection = await this.getCollection();
-    if (!collection) {
-      console.error('Failed to load collection.');
-      throw new Error('No item found.');
-    }
-
-    return collection.findOne({ _id: new ObjectId(id)});
+  private async getCollection() {
+    return;
   }
 
   async load(limit = 10) {
-    const collection = await this.getCollection();
-    if (!collection) {
-      console.error('Failed to load collection.');
-      return [];
-    }
+    return [{
+      name: 'Fake data',
+      currentBid: 20,
+      currentBidder: 'no one',
+    }];
+  }
 
-    return collection?.aggregate([
-      { $limit: limit }
-    ]);
+  async findOne(id: any) {
+    return {};
   }
 
   async getCollectionWatcher(ids: any[]) {
-    if (!ids.length) {
-      return;
-    }
-
-    const collection = await this.getCollection();
-    if (!collection) {
-      return;
-    }
-
-    const objectIds = ids.map(id => new ObjectId(id));
-
-    const generator = collection.watch({ ids: objectIds });
-    return fromChangeEvent(generator).pipe(extractUpdate);
+    return from([]);
   }
 
-  async bid(auction: Auction, username: string, increment: number = 1) {
-    if (!isInUsernamesList(username)) {
-      return;
-    }
+  async bid(
+    auction: Auction,
+    username: string,
+    increment: number = 1
+  ) {
+    // if (!isInUsernamesList(username)) {
+    //   return;
+    // }
 
-    const collection = await this.getCollection();
-    await collection?.findOneAndUpdate({
-      _id: auction._id,
-      currentBid: auction.currentBid,
-      currentBidder: auction.currentBidder
-    }, {
-      $inc: { currentBid: increment },
-      $set: { currentBidder: username }
-    });
-  }
-
-  private async getCollection() {
-    const app = await this.realmAppService.getAppInstance();
-    const mongo = app.currentUser?.mongoClient('mongodb-atlas');
-    const collection = mongo?.db('auctions').collection<Auction>('items');
-
-    return collection;
+    // const collection = await this.getCollection();
+    // await collection?.findOneAndUpdate({
+    //   _id: auction._id,
+    //   currentBid: auction.currentBid,
+    //   currentBidder: auction.currentBidder
+    // }, {
+    //   $inc: { currentBid: increment },
+    //   $set: { currentBidder: username }
+    // });
   }
 }
+
+
+
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuctionService {
+//   constructor(private realmAppService: RealmAppService) {
+//   }
+
+//   async findOne(id: any) {
+//     const collection = await this.getCollection();
+//     if (!collection) {
+//       console.error('Failed to load collection.');
+//       throw new Error('No item found.');
+//     }
+
+//     return collection.findOne({ _id: new ObjectId(id)});
+//   }
+
+//   async load(limit = 10) {
+//     const collection = await this.getCollection();
+//     if (!collection) {
+//       console.error('Failed to load collection.');
+//       return [];
+//     }
+
+//     return collection?.aggregate([
+//       { $limit: limit }
+//     ]);
+//   }
+
+//   async getCollectionWatcher(ids: any[]) {
+//     if (!ids.length) {
+//       return;
+//     }
+
+//     const collection = await this.getCollection();
+//     if (!collection) {
+//       return;
+//     }
+
+//     const objectIds = ids.map(id => new ObjectId(id));
+
+//     const generator = collection.watch({ ids: objectIds });
+//     return fromChangeEvent(generator).pipe(extractUpdate);
+//   }
+
+//   async bid(auction: Auction, username: string, increment: number = 1) {
+//     if (!isInUsernamesList(username)) {
+//       return;
+//     }
+
+//     const collection = await this.getCollection();
+//     await collection?.findOneAndUpdate({
+//       _id: auction._id,
+//       currentBid: auction.currentBid,
+//       currentBidder: auction.currentBidder
+//     }, {
+//       $inc: { currentBid: increment },
+//       $set: { currentBidder: username }
+//     });
+//   }
+
+//   private async getCollection() {
+//     const app = await this.realmAppService.getAppInstance();
+//     const mongo = app.currentUser?.mongoClient('mongodb-atlas');
+//     const collection = mongo?.db('auctions').collection<Auction>('items');
+
+//     return collection;
+//   }
+// }
--- src/app/realm-app.service.ts
+++ src/app/realm-app.service.ts
@@ -7,32 +7,14 @@ import { Injectable } from '@angular/core';
 export class RealmAppService {
   private static app: Realm.App;
 
-  constructor() { }
-
-  async getAppInstance() { 
-    if (RealmAppService.app) {
-      return RealmAppService.app;
-    } else {
+  async getAppInstance() {
+    if (!RealmAppService.app) {
       RealmAppService.app = new Realm.App({ id: 'auctions-wjdls' });
-      await this.loginAnonymous(RealmAppService.app);
 
-      return RealmAppService.app;
+      const credentials = Realm.Credentials.anonymous();
+      await RealmAppService.app.logIn(credentials);
     }
-  }
 
-  private async loginAnonymous(app: Realm.App) {
-    // Create an anonymous credential
-    const credentials = Realm.Credentials.anonymous();
-    try {
-      // Authenticate the user
-      const user = await app.logIn(credentials);
-
-      // `App.currentUser` updates to match the logged in user
-      console.assert(user.id === app?.currentUser?.id);
-      return user;
-    } catch (err) {
-      console.error(err);
-      throw new Error('Failed to log into the Realm app');
-    }
+    return RealmAppService.app;
   }
 }
